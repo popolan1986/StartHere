0174dbb12e67c8b182d5502861598bfc03e70b70
popolan1986 <popo_lan1986@163.com>
2018-01-29 21:51:56 +0800

    Fetch Multisim Live's icon to check if the browser is online

--- src/starthere.js
+++ src/starthere.js
@@ -3,21 +3,7 @@ window.startHere.onLine = true;
 
 function checkNetwork(callback) {
     var xhr = new XMLHttpRequest();
-    // todo: replace this file with the real file in the measurements live server
-    var url = '/resources/checkNetwork.txt';
-    // We don't need to fetch the content
-    xhr.open('HEAD', url);
-    // In IE, the timeout property may be set only after calling open method and before calling the send method
-    xhr.timeout = 3000;
-
-    xhr.ontimeout = function(e) {
-        xhr.abort();
-        if (!window.startHere.onLine) {
-            window.startHere.onLine = false;
-        }
-        callback();
-    }
-
+    // onreadystatechange event handler must be specified before calling the open method
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) {
             return;
@@ -35,6 +21,24 @@ function checkNetwork(callback) {
 
         callback();
     };
+
+    // todo: replace this file with the real file in the measurements live server
+    // The measurements live server must enable CORS.
+    //var url = 'http://127.0.0.1:8081/favicon.ico';
+    var url = 'https://www.multisim.com/favicon.ico'
+    // We don't need to fetch the content
+    xhr.open('HEAD', url);
+    // In IE, the timeout property may be set only after calling open method and before calling the send method
+    xhr.timeout = 3000;
+
+    xhr.ontimeout = function(e) {
+        xhr.abort();
+        if (window.startHere.onLine) {
+            window.startHere.onLine = false;
+        }
+        callback();
+    };
+
     xhr.send();
 }
 
